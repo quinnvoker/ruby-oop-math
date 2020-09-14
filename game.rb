d1c9370153ac78e27bd610985b071362fbaf98f5
Quinn <quinnvoker@gmail.com>
2020-09-14 14:34:23 -0700

    question -> response system works

--- game.rb
+++ game.rb
@@ -10,4 +10,27 @@ class Game
     @players = [@player_1, @player_2]
   end
 
+  def ask_question
+    question = Question.new
+    puts "#{players[0].name}: #{question}"
+
+    print "> "
+    answer = gets.chomp.to_i
+
+    correct = question.is_answer?(answer)
+    if !correct
+      players[0].damage
+    end
+
+    show_response(players[0], correct)
+  end
+
+  def show_response(player, correct)
+    if correct
+      puts "#{player.name}: YES! You are correct."
+    else 
+      puts "#{player.name}: Seriously? No!"
+    end
+  end
+
 end
--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require './game'
 
 game = Game.new
+game.ask_question
 puts game.inspect
